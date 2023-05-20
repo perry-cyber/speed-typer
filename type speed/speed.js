1011fc021dd990538621255aeaddfd89c0224ac2
victor simon <121669086+perry-cyber@users.noreply.github.com>
2023-05-20 11:59:40 +0100

    Add files via upload

---
+++ type speed/speed.js
@@ -0,0 +1,122 @@
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game-container");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
+
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+  "structure",
+  "giant",
+  "battle",
+  "saint",
+  "blended",
+  "married",
+  "particles",
+  "peace",
+  "nigeria",
+  "kingdom",
+  "village",
+  "principal",
+  "streams",
+  "Antiona",
+];
+
+let randomWord;
+
+let score = 0;
+
+let difficulty =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("dificulty")
+    : "medium";
+
+difficultySelect.value =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+let time = 10;
+text.focus();
+
+const timeInterval = setInterval(updateTime, 1120);
+
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameOver();
+  }
+}
+
+function gameOver() {
+  endgameEl.innerHTML = `
+  <h1>Time ran out</h1>
+  <h5> your final score is ${score}</p> 
+  <button onclick="location.reload()">Restart</button>
+  `;
+  endgameEl.style.display = "flex";
+}
+
+addWordToDOM();
+
+text.addEventListener("input", (e) => {
+  const insertedText = e.target.value;
+
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+
+    e.target.value = "";
+    if (difficulty === "hard") {
+      time += 2;
+    } else if (difficulty === "medium") {
+      time += 4;
+    } else time += 6;
+    updateTime();
+  }
+});
+
+settingsBtn.addEventListener("click", () => settings.classList.toggle("hide"));
+
+settingsForm.addEventListener("change", (e) => {
+  difficulty = e.target.value;
+  localStorage.setItem("difficulty", difficulty);
+});
